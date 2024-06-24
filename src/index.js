a062e96b78dcacafa2dd68ae53cdf45087bf1951
MclPio <113801014+MclPio@users.noreply.github.com>
2024-06-24 00:38:06 -0400

    Init home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hi from src/index.js!')
+import Pizza from './pizza.jpg'
+
+const myImage = new Image();
+myImage.src = Pizza;
+myImage.width = '600'
+myImage.alt = 'A cheese pizza with golden crust, cherry tomatoes, rosemary, and a large yellow tomato on a wooden surface.'
+const element = document.getElementById('content');
+element.appendChild(myImage);
