55bab745721c02222fed6ecd25172aa1c131c87c
MclPio <113801014+MclPio@users.noreply.github.com>
2024-06-25 21:26:27 -0400

    Save work

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import { homepage } from './home.js';
 import { menu } from './menu.js';
 import { about } from './about.js';
+import './style.css';
 
 homepage();
 const navbar = document.getElementById('navbar');
 const content = document.getElementById('content');
 
 navbar.addEventListener('click', (event) => {
-  content.innerHTML = '';
 
   if (event.target.id == 'home') {
+    content.innerHTML = '';
     homepage();
   } else if (event.target.id == 'menu') {
+    content.innerHTML = '';
     menu();
   } else if (event.target.id == 'about') {
+    content.innerHTML = '';
     about();
   };
 })
