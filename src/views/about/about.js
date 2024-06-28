32ca9bce9cf57c97f6959618d411fd811666dbec
MclPio <113801014+MclPio@users.noreply.github.com>
2024-06-28 13:15:11 -0400

    Complete app

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -1,21 +1,28 @@
 function about() {
   const content = document.getElementById('content');
   const node = document.createElement('div');
-  node.className = 'contact'
+  node.className = 'section'
+  const container = document.createElement('div');
+  container.className = 'container';
 
   const name = document.createElement('h2');
-  name.innerText = 'Michael';
+  name.innerText = 'Miharu | みはる';
+  name.className = 'chef-name'
 
   const phone = document.createElement('p');
   phone.innerText = '123 123 1234';
+  phone.className = 'chef-details';
 
   const email = document.createElement('p');
-  email.innerText = 'michael@world.pizza'
+  email.innerText = 'miharu@world.pizza'
+  email.className = 'chef-details';
 
   const role = document.createElement('p');
   role.innerText = 'Head Chef';
+  role.className = 'chef-details';
 
-  node.append(name, role, phone, email);
+  container.append(name, role, phone, email)
+  node.append(container);
   content.appendChild(node);
 }
 
--- src/views/menu/menu.js
+++ src/views/menu/menu.js
@@ -2,34 +2,45 @@ import menuData from './menu.json';
 
 function createMenuItem(title, desc, price, imgSrc=null, imgDesc=null) {
   const node = document.createElement('li')
+  const titlePrice = document.createElement('div');
+  titlePrice.className = 'title-price';
 
   const titleEle = document.createElement('h3');
   titleEle.innerHTML = `<strong>${title}</strong>`;
+  titleEle.className = 'roboto';
 
   const descEle = document.createElement('p');
+  descEle.className = 'item-desc'
   descEle.innerText = desc;
 
   const priceEle = document.createElement('p');
+  priceEle.className = 'item-price'
   priceEle.innerHTML = `$${price.toFixed(2)}`;
-
+  
+  titlePrice.append(titleEle, priceEle);
   if (imgSrc) {
     const imgEle = new Image();
     imgEle.src = imgSrc;
     imgEle.alt = imgDesc;
-    node.append(titleEle, descEle, priceEle, imgEle);
+    node.append(titlePrice, descEle, imgEle);
     return node;
   };
   
-  node.append(titleEle, descEle, priceEle);
+  node.append(titlePrice, descEle );
   return node;
 }
 
 function menu() {
   const content = document.getElementById('content');
+  const section = document.createElement('section');
+  const container = document.createElement('div');
+  container.className = 'container'
+  section.className = 'section';
 
   const header = document.createElement('h1');
-  header.innerText = 'Delicious is not fictitious';
-  content.appendChild(header);
+  header.innerText = 'Delicious Is Not Fictitious';
+  header.className = 'menu-heading-1 title is-2';
+  container.appendChild(header);
 
   const menuContainer = document.createElement('div');
   menuContainer.id = 'menu';
@@ -37,6 +48,7 @@ function menu() {
   menuData.menu.forEach(category => {
     const categoryTitle = document.createElement('h2');
     categoryTitle.textContent = category.category;
+    categoryTitle.className = 'menu-heading-2 title is-3'
     menuContainer.appendChild(categoryTitle);
 
     const itemList = document.createElement('ul');
@@ -47,8 +59,9 @@ function menu() {
 
     menuContainer.appendChild(itemList);
   });
-
-  content.appendChild(menuContainer);
+  container.appendChild(menuContainer);
+  section.appendChild(container);
+  content.appendChild(section);
 }
 
 export { menu };
